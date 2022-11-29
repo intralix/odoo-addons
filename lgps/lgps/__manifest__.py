ca2ffad1aa83fa97f7459938856694033270b014
javina-lmbl <jorge.avina@logicamobile.com>
2022-11-29 16:04:40 -0600

    chore: Basic module structure

---
+++ lgps/lgps/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'lgps',
+    'description': 'Intralix module for internal processes',
+    'author': 'Intralix',
+    'application': True,
+    'website': 'https://www.intralix.com',
+    'category': 'Uncategorized',
+    'version': '0.0.0',
+    'depends': [
+        'base',
+        'stock',
+        'contacts',
+        'account',
+        'repair',
+        'crm',
+        'sale_subscription',
+        'helpdesk',
+        'mail',
+        'project',
+        'sale'
+    ],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+    ],
+}
